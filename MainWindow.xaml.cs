7631a290623870d6b9885882eae806b544aa235a
LZonca <leo.zonca@business-school.academy>
2024-06-18 03:23:06 +0200

    First commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,47 @@ namespace LaravelLauncher
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        static void RunCommand(string command)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd", "/c " + command); //TODO: modifier le path
+
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.CreateNoWindow = true;
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = processStartInfo;
+                process.Start();
+
+                string result = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+
+                Console.WriteLine(result);
+            }
+        }
+
+
+        private void StartProjectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void SelectProjectPath_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StartLocalServerBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
